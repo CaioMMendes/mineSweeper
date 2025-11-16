53bb9678813f4f8f00d1d5ed69549dfda2fa3cc8
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2025-11-16 13:33:01 -0300

    feat: add button component

---
+++ src/app/components/button.tsx
@@ -0,0 +1,10 @@
+import { ButtonHTMLAttributes } from "react"
+
+export function Button({ ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-md bg-zinc-600 text-zinc-50`}
+      {...rest}
+    ></button>
+  )
+}
