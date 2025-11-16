8a83e8e75b4685b60f07ddc5f7ced743a227acc4
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2025-11-16 13:33:19 -0300

    feat: add useBoard hook

---
+++ src/app/hooks/useBoard.ts
@@ -0,0 +1,193 @@
+import { useState } from "react"
+
+type BoardDifficulty = "easy" | "medium" | "hard"
+
+const boardStats = {
+  "easy": {
+    numberOfBombs: 5,
+    size: 5,
+  },
+  "medium": {
+    numberOfBombs: 15,
+    size: 7,
+  },
+  "hard": {
+    numberOfBombs: 10,
+    size: 9,
+  },
+}
+
+export function useBoard(difficulty: BoardDifficulty) {
+  const [board, setBoard] = useState(() => generateBoard(difficulty))
+  const [opened, setOpened] = useState(() => generateOpenedCells(difficulty))
+  const [marked, setMarked] = useState(() => generateMarkedCells(difficulty))
+  const [isEndGame, setIsEndGame] = useState(false)
+
+  function openCell(coord: number[]) {
+    const [i, j] = coord
+    const visited = new Set<string>()
+    let stop = false
+
+    open([i, j])
+
+    function open([i, j]: number[]) {
+      const key = `${i}-${j}`
+
+      if (visited.has(key)) return
+      visited.add(key)
+      if (board?.[i]?.[j] === undefined || opened[key] || stop) return //não existe essa coordenada
+
+      if (board[i][j] === -1) {
+        stop = true
+        setIsEndGame(true)
+        setOpened((opened) => {
+          const newOpened: Record<string, boolean> = {}
+          for (const objectKey in opened) {
+            newOpened[objectKey] = true
+          }
+          return newOpened
+        })
+
+        return
+      }
+      if (board[i][j] !== 0) {
+        setOpened((opened) => {
+          return { ...opened, [key]: true }
+        })
+        return
+      }
+      setOpened((opened) => {
+        return { ...opened, [key]: true }
+      })
+
+      open([i + 1, j])
+      open([i - 1, j])
+      open([i + 1, j - 1])
+      open([i + 1, j + 1])
+      open([i - 1, j + 1])
+      open([i - 1, j - 1])
+      open([i, j - 1])
+      open([i, j + 1])
+    }
+  }
+
+  function resetGame() {
+    setOpened((opened) => {
+      const newOpened: Record<string, boolean> = {}
+      for (const objectKey in opened) {
+        newOpened[objectKey] = false
+      }
+      return newOpened
+    })
+    setMarked((marked) => {
+      const newMarked: Record<string, boolean> = {}
+      for (const objectKey in marked) {
+        newMarked[objectKey] = false
+      }
+      return newMarked
+    })
+    setBoard(() => generateBoard(difficulty))
+  }
+
+  function handleMarkCell([i, j]: number[]) {
+    setMarked((marked) => {
+      return { ...marked, [`${i}-${j}`]: !marked[`${i}-${j}`] }
+    })
+  }
+
+  return {
+    board,
+    opened,
+    openCell,
+    isEndGame,
+    resetGame,
+    marked,
+    handleMarkCell,
+  }
+}
+
+function generateBoard(difficulty: BoardDifficulty) {
+  const { numberOfBombs, size } = boardStats[difficulty]
+
+  const board: number[][] = []
+  for (let i = 0; i < size; i++) {
+    const array = []
+    for (let j = 0; j < size; j++) {
+      array.push(0)
+    }
+    board.push(array)
+  }
+
+  let count = 0
+  while (count < numberOfBombs) {
+    const [x, y] = randomCoordenate(size)
+    if (board[x][y] === -1) continue
+
+    board[x][y] = -1
+    count++
+  }
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (board[i][j] === -1) continue
+
+      const topMiddle = board?.[i + 1]?.[j] === -1 ? 1 : 0
+      const topLeft = board?.[i + 1]?.[j - 1] === -1 ? 1 : 0
+      const topRight = board?.[i + 1]?.[j + 1] === -1 ? 1 : 0
+      const left = board?.[i]?.[j - 1] === -1 ? 1 : 0
+      const right = board?.[i]?.[j + 1] === -1 ? 1 : 0
+      const bottomMiddle = board?.[i - 1]?.[j] === -1 ? 1 : 0
+      const bottomLeft = board?.[i - 1]?.[j - 1] === -1 ? 1 : 0
+      const bottomRight = board?.[i - 1]?.[j + 1] === -1 ? 1 : 0
+
+      const count =
+        topMiddle +
+        topLeft +
+        topRight +
+        left +
+        right +
+        bottomLeft +
+        bottomMiddle +
+        bottomRight
+
+      board[i][j] = count
+    }
+  }
+
+  return board
+}
+
+function randomCoordenate(size: number) {
+  const randomX = Math.floor(Math.random() * size)
+  const randomY = Math.floor(Math.random() * size)
+  return [randomX, randomY]
+}
+
+function generateOpenedCells(difficulty: BoardDifficulty) {
+  const { size } = boardStats[difficulty]
+  const openList: Record<string, boolean> = {}
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      openList[`${i}-${j}`] = false
+    }
+  }
+
+  return openList
+}
+
+function generateMarkedCells(difficulty: BoardDifficulty) {
+  const { size } = boardStats[difficulty]
+  const markedList: Record<string, boolean> = {}
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      markedList[`${i}-${j}`] = false
+    }
+  }
+
+  return markedList
+}
+
+//todo fazer quando perde
+//todo fazer verificação se a pessoa ganhou
