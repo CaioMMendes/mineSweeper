fae09a038ada12d542f4bc698f402d7ef36e99e5
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2026-01-25 18:04:22 -0300

    feat: add countNumberOfFlagsRound function

---
+++ src/app/hooks/useBoard/functions/countNumberOfFlagsRounding.ts
@@ -0,0 +1,19 @@
+export function countNumberOfFlagsRounding(
+  coord: number[],
+  marked: Record<string, boolean>,
+) {
+  let count = 0
+
+  const [i, j] = coord
+
+  if (marked[`${i + 1}-${j}`]) count++
+  if (marked[`${i - 1}-${j}`]) count++
+  if (marked[`${i + 1}-${j - 1}`]) count++
+  if (marked[`${i + 1}-${j + 1}`]) count++
+  if (marked[`${i - 1}-${j + 1}`]) count++
+  if (marked[`${i - 1}-${j - 1}`]) count++
+  if (marked[`${i}-${j - 1}`]) count++
+  if (marked[`${i}-${j + 1}`]) count++
+
+  return count
+}
