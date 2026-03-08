64591a5cec0a16155358f1f2222427627a995bec
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2026-03-07 21:34:13 -0300

    feat: add scores route

---
+++ src/app/api/scores/route.ts
@@ -0,0 +1,45 @@
+import { RANKING_LIMIT } from "@/app/constants/rankingLimitConstant"
+import { prisma } from "@/lib/prisma"
+import { Difficulty } from "@prisma/client"
+
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  const score = await prisma.score.create({
+    data: {
+      playerName: body.playerName,
+      timeMs: body.timeMs,
+      difficulty: body.difficulty as Difficulty,
+      gameVersion: body.version,
+    },
+  })
+
+  console.log(score)
+
+  return Response.json(score)
+}
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url)
+
+  const difficulty = searchParams.get("difficulty") as Difficulty
+  const version = searchParams.get("version")
+  const limit = Number(searchParams.get("limit") ?? RANKING_LIMIT)
+
+  const ranking = await prisma.score.findMany({
+    where: {
+      difficulty,
+      gameVersion: version!,
+    },
+    orderBy: {
+      timeMs: "asc",
+    },
+    take: limit,
+    select: {
+      playerName: true,
+      timeMs: true,
+    },
+  })
+
+  return Response.json(ranking)
+}
